d81943ea05e0b4de8a06aca3f65dbbfb200ddebd
lucas <lucascudeler37@gmail.com>
2025-03-07 12:53:23 -0300

    feat: divisao

--- menu.py
+++ menu.py
@@ -7,3 +7,8 @@ def subtrair(a, b):
 def multiplicar(a, b):
     return a * b
 
+def dividir(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Não é possível dividir por zero!"
